99d117f981e58ea6a83cfa96a678b71a4ad7f0a5
Aditya Mahajan <Adityaspmahajan@gmail.com>
2026-05-24 13:56:59 +0530

    Deploy: Vite config update for custom url

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { resolve } from 'node:path'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: '/learning-spanish/',
+  base: '/',
   build: {
     outDir: 'dist',
     rollupOptions: {
